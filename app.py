b9225936c2574a867fcecdffd8be3051939b8bbc
komfiGolabi <komfalos1@gmail.com>
2021-10-12 14:52:25 +0000

    created a base template

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
- 
+
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
@@ -15,6 +15,7 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
+
 
 @app.route("/")
 @app.route("/get_concerts")
