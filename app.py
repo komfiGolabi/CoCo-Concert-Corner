05b0494e3a6e710dc6cdfb41cdba54380cb77bb6
komfiGolabi <komfalos1@gmail.com>
2021-10-19 16:41:00 +0000

    added the registration function

--- app.py
+++ app.py
@@ -27,6 +27,24 @@ def get_concerts():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # checking if the username exists in database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("You have succesfully registered to CoCo!")
     return render_template("register.html")
 
 
