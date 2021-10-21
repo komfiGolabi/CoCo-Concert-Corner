35dc7482143f54916005bd579a1d0495a8daff0f
komfiGolabi <komfalos1@gmail.com>
2021-10-21 10:57:32 +0000

    build a login functionality

--- app.py
+++ app.py
@@ -48,6 +48,31 @@ def register():
     return render_template("register.html")
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # if user exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # if password is correct
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                # invalid password match
+                flash("Oops, your password/username is incorrect!")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Oops, your password/username is incorrect!")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
