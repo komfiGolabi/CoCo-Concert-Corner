cfc4d7f64d44757bc7e38e6f5a840a3cfce1f906
komfiGolabi <komfalos1@gmail.com>
2021-11-19 21:29:53 +0000

    changed debug to False

--- app.py
+++ app.py
@@ -247,4 +247,4 @@ def add_review():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
